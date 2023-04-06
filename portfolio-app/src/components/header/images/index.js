8fce8b36d6646e87d1754b3290607e49f2ea5acd
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-04-06 04:53:01 -0400

    gh-pages update

--- portfolio-app/src/components/header/images/index.js
+++
@@ -1,8 +0,0 @@
-const socialImages = {
-  gitHub: require("./github-logo.webp"),
-  linkedIn: require("./linkedin-logo.webp"),
-  twitter: require("./twitter-logo.webp"),
-  discord: require("./discord-logo.webp"),
-};
-
-export { socialImages };
