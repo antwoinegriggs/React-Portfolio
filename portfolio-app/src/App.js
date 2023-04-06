5962b7df63bd09080a2cfcafcac2e7305b226ffb
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-04-06 02:24:51 -0400

    HashRouter

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  HashRouter,
-} from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/routes/home.js";
 import Contact from "./components/routes/contact.js";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
-        </Router>
-      </HashRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </div>
   );
 }
